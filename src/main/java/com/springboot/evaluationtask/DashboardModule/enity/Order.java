ded73694e3a55cdbc0ea1d5e3ec4063dcb5d510b
KeerthanaDeviVenkatesan <131868551+KeerthanaDeviVenkatesan@users.noreply.github.com>
2023-10-24 11:06:46 +0530

    Order.java

--- src/main/java/com/springboot/evaluationtask/DashboardModule/enity/Order.java
+++ src/main/java/com/springboot/evaluationtask/DashboardModule/enity/Order.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,14 +20,27 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Long accId;
-    private String stockSymbol;
-    private String orderType;
-    private int quantity;
-    private double price;
-    private String status;
-    private String timestamp;
 
+    @NotNull(message = "Account ID cannot be null")
+    private Long accId;
+
+    @NotBlank(message = "Stock symbol cannot be blank")
+    private String stockSymbol;
+
+    @NotBlank(message = "Order type cannot be blank")
+    private String orderType;
+
+    @Positive(message = "Quantity must be a positive number")
+    private int quantity;
+
+    @Positive(message = "Price must be a positive number")
+    private double price;
+
+    @NotBlank(message = "Status cannot be blank")
+    private String status;
+
+    @NotBlank(message = "Timestamp cannot be blank")
+    private String timestamp;
 
     @ManyToOne
     @JoinColumn(name = "userId")
