e848aff9609e990cfce4fffaf6db4eab7d5ffff9
KeerthanaDeviVenkatesan <131868551+KeerthanaDeviVenkatesan@users.noreply.github.com>
2023-10-31 12:02:33 +0530

    Update QuoteService.java

--- src/main/java/com/springboot/evaluationtask/QuoteModule/service/QuoteService.java
+++ src/main/java/com/springboot/evaluationtask/QuoteModule/service/QuoteService.java
@@ -1,7 +1,12 @@
 package com.springboot.evaluationtask.QuoteModule.service;
 
 import com.springboot.evaluationtask.DashboardModule.enity.Symbol;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface QuoteService {
-    Symbol getSymbolDetails(String symbol);
+     Symbol saveSymbol(Symbol symbol);
+    Symbol getSymbolById(Long symbolId);
+    List<Symbol> uploadSymbolDetails(MultipartFile file);
 }
