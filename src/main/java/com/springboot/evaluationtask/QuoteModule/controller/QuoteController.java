779313afec999db7b4c660f9fb2a6617182f5ed0
KeerthanaDeviVenkatesan <131868551+KeerthanaDeviVenkatesan@users.noreply.github.com>
2023-10-31 12:04:00 +0530

    Update QuoteController.java

--- src/main/java/com/springboot/evaluationtask/QuoteModule/controller/QuoteController.java
+++ src/main/java/com/springboot/evaluationtask/QuoteModule/controller/QuoteController.java
@@ -5,30 +5,38 @@ import com.springboot.evaluationtask.QuoteModule.service.Impl.QuoteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/quote")
 public class QuoteController {
     @Autowired
-    QuoteServiceImpl quoteService;
-    @GetMapping("/symbolDetails/{symbol}")
-    public ResponseEntity<String> getSymbolDetails(@PathVariable String symbol) {
-        try {
-            // Call the service to fetch symbol details
-            Symbol symbol1 = quoteService.getSymbolDetails(symbol);
+    private QuoteServiceImpl symbolService;
 
-            if (symbol1 != null) {
-                return ResponseEntity.ok("displayed"+symbol1);
-            } else {
-                return ResponseEntity.notFound().build(); // Symbol not found
-            }
-        } catch (Exception e) {
-            return ResponseEntity.ok("not fetched");
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadSymbolDetails(@RequestParam("file") MultipartFile file) {
+        List<Symbol> uploadedSymbols = symbolService.uploadSymbolDetails(file);
+
+        if (!uploadedSymbols.isEmpty()) {
+            return ResponseEntity.ok("Symbols uploaded successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error uploading symbols or no valid symbols found in the file.");
         }
     }
 
+    @GetMapping("/{symbolId}")
+    public ResponseEntity<Symbol> getSymbolDetails(@PathVariable Long symbolId) {
+        Symbol symbol = symbolService.getSymbolById(symbolId);
+
+        if (symbol != null) {
+            return ResponseEntity.ok(symbol);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
 }
